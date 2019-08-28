90fe6fc1dbe4df40db564624608a369a7b3ddce0
Patriot720 <cerkin-3@yandex.ru>
2019-08-28 21:45:41 +0300

    fix

--- src/collections_musescore/db.cljs
+++ src/collections_musescore/db.cljs
@@ -1,7 +1,7 @@
 (ns collections-musescore.db
-(:require
- [re-frame.core :refer [dispatch dispatch-sync reg-cofx]]
- [cljs.spec.alpha :as s])
+  (:require
+   [re-frame.core :refer [dispatch dispatch-sync reg-cofx]]
+   [cljs.spec.alpha :as s]))
 
 (def ::collections (s/and
                     (s/map-of ::id ::todo)
