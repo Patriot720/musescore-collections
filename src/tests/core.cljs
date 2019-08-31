02b999682e48c286e1d5721b501c42db1983b7cc
Patriot720 <cerkin-3@yandex.ru>
2019-08-31 21:40:17 +0300

    tests

---
+++ src/tests/core.cljs
@@ -0,0 +1,6 @@
+(ns collections-musescore.tests.core
+(:require [cljs.test :refer-macros [run-tests]])
+  )
+
+(deftest failing-test
+  (is (= 0 1)))
---
+++ src/tests/test_runner.cljs
@@ -0,0 +1,13 @@
+(ns collections-musescore.tests.test-runner
+  (:require [cljs.test :refer-macros [run-tests]]
+            [tests.core]))
+
+;; This isn't strictly necessary, but is a good idea depending
+;; upon your application's ultimate runtime engine.
+(enable-console-print!)
+
+(defn run-all-tests
+  []
+  (run-tests
+   'tests.core))
+
