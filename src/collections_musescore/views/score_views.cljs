b1c57e3636d028d1eb9131e2d8828946b1ab1876
Patriot720 <cerkin-3@yandex.ru>
2019-10-08 20:49:39 +0300

    minor score-view changes

--- src/collections_musescore/views/score_views.cljs
+++ src/collections_musescore/views/score_views.cljs
@@ -2,33 +2,50 @@
   (:require ["@material-ui/core" :as mui]
             [re-frame.core :refer [subscribe dispatch]]
             ["@material-ui/icons" :as mui-icons]))
-
+(defn score-info-item [icon text]
+  [:> mui/Grid {:item true
+                :display "flex"
+                :alignItems "center"}
+   icon text])
 (defn score-info [{:keys [favorite-count
                           playback-count
                           tags]}]
-  [:> mui/Grid {:container true}
-   [:> mui/Grid {:item true}
-    [:> mui-icons/Accessible] favorite-count]
-   [:> mui/Grid {:item true}
-    [:> mui-icons/Accessible] playback-count]
-   [:> mui/Grid {:item true} ; TODO to a loop
-    [:> mui-icons/Accessible] tags]])
+
+  [:> mui/Typography {:variant "body1"}
+   [:> mui/Grid {:container true :spacing 2
+                 :justify "center"}
+    [score-info-item [:> mui-icons/Accessible] favorite-count]
+    [score-info-item [:> mui-icons/Accessible] playback-count]
+    [score-info-item [:> mui-icons/Accessible] tags]]])
 
 
 (defn score-view [collection-id {:keys [id title url
                                         favorite-count
                                         playback-count
                                         tags]}]
-  [:a {:href url :target "_blank"}
-   [:> mui/Grid {:container true
-                 :alignItems "center"
-                 :justify "space-evenly"}
-    [:> mui/Grid {:item true}
-     [:h1 title]]
-    [:> mui/Grid {:item true}
-     [score-info {:favorite-count favorite-count
-                  :playback-count playback-count
-                  :tags tags}]]
-    [:> mui/Grid {:item true}
-     [:> mui/Button {:on-click
-                     #(dispatch [:remove-score collection-id id])} "DELETE"]]]])
+  [:> mui/Grid {:container true
+                :spacing 2
+                :alignItems "center"
+                :justify "space-evenly"}
+   [:> mui/Grid {:container true :item true
+                 :direction "column" :xs 4
+                 :spacing 2}
+    [:> mui/Typography {:align "center"}
+     [:> mui/Grid {:item true}
+      [:h1  title]]
+     [:> mui/Grid {:item true}
+      [score-info {:favorite-count favorite-count
+                   :playback-count playback-count
+                   :tags tags}]]]]
+   [:> mui/Grid {:item true}
+
+    [:a {:href url :target "_blank"}
+     [:> mui/Button
+      {:variant "contained"
+       :color "primary"}
+      "Go to score"]]]
+   [:> mui/Grid {:item true}
+    [:> mui/Button {:variant "contained"
+                    :color "secondary"
+                    :on-click
+                    #(dispatch [:remove-score collection-id id])} "DELETE"]]])
