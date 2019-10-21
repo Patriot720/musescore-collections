4232c4625eb238aa89128741ade3d05163325703
Patriot720 <cerkin-3@yandex.ru>
2019-10-21 16:01:56 +0300

    remove junk

--- src/collections_musescore/events/interceptors.cljs
+++ src/collections_musescore/events/interceptors.cljs
@@ -1,7 +1,6 @@
 (ns collections-musescore.events.interceptors
   (:require [cljs.spec.alpha :as s]
             [collections-musescore.db :as db]
-            [collections-musescore.api :as api]
             [re-frame.core :refer [after path dispatch ->interceptor]]))
 
 (defn- check-and-throw
@@ -11,13 +10,6 @@
     (throw (ex-info (str "spec check failed: " (s/explain-str a-spec db)) {}))))
 
 (def check-spec-interceptor (after (partial check-and-throw :collections-musescore.db/db)))
-
-(defn- score-interceptor [{{[_ collection-id score-id _ url] :events} :coeffects}]
-  (api/get-info-by-url url #(dispatch [:update-score collection-id score-id %])))
-
-(def add-score-intereptor (->interceptor
-                           :id :add-score-interceptor
-                           :after))
 
 (def ->local-store (after db/->local-store))
 (def collections-interceptors [check-spec-interceptor
